28190cedb8d0b75fa290a9e3fa924d72eb0ed042
Jumane555 <125601997+Jumane555@users.noreply.github.com>
2023-04-05 23:19:03 +0300

    Update phonebook.c

--- phonebook.c
+++ phonebook.c
@@ -1,8 +1,11 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+// This function reads contacts from a file and stores them in the given arrays.
+// It returns the number of contacts read.
 int read_contacts(char** name, char** contacts, int max_contacts, const char* filename) {
+    // Open the file for reading.
     FILE* fp = fopen(filename, "r");
     if (!fp) {
         printf("Error opening file '%s'\n", filename);
@@ -10,7 +13,11 @@ int read_contacts(char** name, char** contacts, int max_contacts, const char* fi
     }
 
     int num_contacts = 0;
+     // Read contacts from the file until we reach the end or the maximum number of contacts.
     while (num_contacts < max_contacts && !feof(fp)) {
+    // Use fscanf to read the name and contact information from the file.
+        // Each field is limited to a certain number of characters to prevent buffer overflow.
+        // If fscanf successfully reads both fields, increment the number of contacts.
         if (fscanf(fp, "%24s %11s", name[num_contacts], contacts[num_contacts]) == 2) {
             num_contacts++;
         }
@@ -20,13 +27,15 @@ int read_contacts(char** name, char** contacts, int max_contacts, const char* fi
     return num_contacts;
 }
 
+// This function writes contacts from the given arrays to a file.
 void write_contacts(char** name, char** contacts, int num_contacts, const char* filename) {
+    // Open the file for writing.
     FILE* fp = fopen(filename, "w");
     if (!fp) {
         printf("Error opening file '%s'\n", filename);
         return;
     }
-
+ // Write each contact
     for (int i = 0; i < num_contacts; i++) {
         fprintf(fp, "%s %s\n", name[i], contacts[i]);
     }
@@ -35,9 +44,11 @@ void write_contacts(char** name, char** contacts, int num_contacts, const char* 
 }
 
 int main() {
+     // Allocate memory for arrays to hold up to 10 contacts.
     char** name = malloc(sizeof(char*) * 10);
     char** contacts = malloc(sizeof(char*) * 10);
-
+   // Allocate memory for each name and contact information field.
+        // Each field is limited to a certain number of characters to prevent buffer overflow.
     for (int i = 0; i < 10; i++) {
         name[i] = malloc(sizeof(char) * 25);
         contacts[i] = malloc(sizeof(char) * 12);
@@ -45,6 +56,8 @@ int main() {
 
     int num_contacts = 0;
     int choice = 0;
+
+    // Read contacts from the file "contacts.txt".
 
     num_contacts = read_contacts(name, contacts, 10, "contacts.txt");
 
@@ -58,7 +71,7 @@ int main() {
         scanf("%d", &choice);
 
         switch (choice) {
-            case 1:
+            case 1: // Add a new contact if there is room in the arrays.
                 if (num_contacts >= 10) {
                     printf("Maximum number of contacts reached.\n");
                 } else {
@@ -72,6 +85,7 @@ int main() {
                 }
                 break;
             case 2:
+                 // Display all contacts in the arrays.
                 printf("\nContacts:\n");
                 for (int i = 0; i < num_contacts; i++) {
                     printf("%s: %s\n", name[i], contacts[i]);
@@ -98,3 +112,4 @@ int main() {
 
     return 0;
 }
+
