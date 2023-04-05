94d1ba9dc432dc03f41fc60cf3e716160ce51a3a
Jumane555 <125601997+Jumane555@users.noreply.github.com>
2023-04-05 23:34:10 +0300

    Update phonebook.c

--- phonebook.c
+++ phonebook.c
@@ -4,7 +4,7 @@
 #include <string.h>
 // This function reads contacts from a file and stores them in the given arrays.
 // It returns the number of contacts read.
-int read_contacts(char** name, char** contacts, int max_contacts, const char* filename) {
+int readcontacts(char** name, char** contacts, int maxcontacts, const char* filename) {
     // Open the file for reading.
     FILE* fp = fopen(filename, "r");
     if (!fp) {
@@ -12,23 +12,23 @@ int read_contacts(char** name, char** contacts, int max_contacts, const char* fi
         return 0;
     }
 
-    int num_contacts = 0;
+    int numberofcontacts = 0;
      // Read contacts from the file until we reach the end or the maximum number of contacts.
-    while (num_contacts < max_contacts && !feof(fp)) {
+    while (numberofcontacts < maxcontacts && !feof(fp)) {
     // Use fscanf to read the name and contact information from the file.
         // Each field is limited to a certain number of characters to prevent buffer overflow.
         // If fscanf successfully reads both fields, increment the number of contacts.
-        if (fscanf(fp, "%24s %11s", name[num_contacts], contacts[num_contacts]) == 2) {
-            num_contacts++;
+        if (fscanf(fp, "%24s %11s", name[numberofcontacts], contacts[numberofcontacts]) == 2) {
+            numberofcontacts++;
         }
     }
 
     fclose(fp);
-    return num_contacts;
+    return numberofcontacts;
 }
 
 // This function writes contacts from the given arrays to a file.
-void write_contacts(char** name, char** contacts, int num_contacts, const char* filename) {
+void writecontacts(char** name, char** contacts, int numberofcontacts, const char* filename) {
     // Open the file for writing.
     FILE* fp = fopen(filename, "w");
     if (!fp) {
@@ -36,7 +36,7 @@ void write_contacts(char** name, char** contacts, int num_contacts, const char* 
         return;
     }
  // Write each contact
-    for (int i = 0; i < num_contacts; i++) {
+    for (int i = 0; i < numberofcontacts; i++) {
         fprintf(fp, "%s %s\n", name[i], contacts[i]);
     }
 
@@ -54,12 +54,12 @@ int main() {
         contacts[i] = malloc(sizeof(char) * 12);
     }
 
-    int num_contacts = 0;
+    int numberofcontacts = 0;
     int choice = 0;
 
     // Read contacts from the file "contacts.txt".
 
-    num_contacts = read_contacts(name, contacts, 10, "contacts.txt");
+    numberofcontacts = readcontacts(name, contacts, 10, "contacts.txt");
 
     do {
         printf("\nMenu:\n");
@@ -72,27 +72,27 @@ int main() {
 
         switch (choice) {
             case 1: // Add a new contact if there is room in the arrays.
-                if (num_contacts >= 10) {
+                if (numberofcontacts >= 10) {
                     printf("Maximum number of contacts reached.\n");
                 } else {
                     printf("\nEnter name: ");
-                    scanf("%24s", name[num_contacts]);
+                    scanf("%24s", name[numberofcontacts]);
 
                     printf("Enter contact information: ");
-                    scanf("%11s", contacts[num_contacts]);
+                    scanf("%11s", contacts[numberofcontacts]);
 
-                    num_contacts++;
+                    numberofcontacts++;
                 }
                 break;
             case 2:
                  // Display all contacts in the arrays.
                 printf("\nContacts:\n");
-                for (int i = 0; i < num_contacts; i++) {
+                for (int i = 0; i < numberofcontacts; i++) {
                     printf("%s: %s\n", name[i], contacts[i]);
                 }
                 break;
             case 3:
-                write_contacts(name, contacts, num_contacts, "contacts.txt");
+                writecontacts(name, contacts, numberofcontacts, "contacts.txt");
                 printf("Contacts saved to file 'contacts.txt'\n");
                 break;
             case 4:
